c4bc6fe947a362a10bbcd915ff4ce85f137e1815
Desmond Sofua <mrsofua@gmail.com>
2023-09-23 15:36:18 +0000

    feat: v3 done

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -8,65 +8,17 @@ export const Button = styled("button", {
   display: "flex",
   alignItems: "center",
   justifyContent: "center",
-  height: "calc(100% - 2px)",
-  gap: 40,
-  border: "1px solid #8C6CBC",
-  backgroundColor: "#1A0F2B",
+  backgroundColor: "#251B2F",
   color: "#D1CFD5",
-  padding: 24,
+  padding: 20,
   borderRadius: 8,
-  fontSize: 16,
-  fontWeight: "$forzaMedium",
-  width: "auto",
   cursor: "pointer",
   transition: "all .2s ease",
-
-  // "@sm": {
-  //   width: "calc(100% - 40px - 2px)",
-  // },
 
   "&:disabled": {
     backgroundColor: "#1A0F2B",
     color: "#D1CFD5",
     opacity: 0.5,
     cursor: "not-allowed",
-
   },
-
-  variants: {
-    filled: {
-      true: {
-        border: "none",
-        justifyContent: "center",
-        backgroundColor: "#1A0F2B",
-        // padding: "49px 40px",
-        // fontSize: 24,
-        // gap: 250,
-        // "@sm": {
-        //   width: "calc(100% -  - 2px)",
-        //   padding: "32px 20px",
-        //   gap: 0,
-        //   fontSize: 16,
-        // },
-        // "&:hover": {
-        //   color: "$primaryBackground",
-        //   background: "transparent",
-        // },
-      },
-    },
-    unstyled: {
-      true: {
-        border: "none",
-        justifyContent: "space-between",
-        backgroundColor: "transparent",
-        padding: "unset",
-        width: "max-content",
-      },
-    },
-  },
-
-  // "&:hover": {
-  //   color: "$primaryBackground",
-  //   background: "$primaryText",
-  // },
 });
---
+++ pages/lenses.tsx
@@ -0,0 +1,58 @@
+import { Column, PageMeta } from "@/components";
+import { Button } from "@/components/Button";
+import { styled } from "@/stitches.config";
+import { ArrowLeft2 } from "iconsax-react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import { Forza } from "@/fonts/fonts";
+
+const lenses = [
+  {
+    title: "SPAWN After-Fire",
+    link: "https://t.snapchat.com/TiU0P2CK",
+    image: "/lenses/afterfire.png",
+  },
+];
+
+export default function LensesPage() {
+  const { back } = useRouter();
+  return (
+    <PageMeta>
+      <Wrapper className={Forza.className}>
+        <Button onClick={back}>
+          <ArrowLeft2 color="#E7DEBF" />
+        </Button>
+        <Grid>
+          {lenses.map((lens, index) => (
+            <Column key={index} alignItems="center" css={{ gap: 20 }}>
+              <Link href={lens.link} target="_blank" prefetch>
+                <img src={lens.image} alt={lens.title} />
+              </Link>
+              <ForzaText>{lens.title}</ForzaText>
+            </Column>
+          ))}
+        </Grid>
+      </Wrapper>
+    </PageMeta>
+  );
+}
+
+const Wrapper = styled("main", {
+  padding: "70px 16px",
+  maxWidth: 426,
+  marginInline: "auto",
+});
+
+const Grid = styled("div", {
+  display: "grid",
+  gridTemplateColumns: "1fr 1fr",
+  gap: "20px 0px",
+  marginTop: 20,
+});
+
+const ForzaText = styled("p", {
+  fontSize: 18,
+  fontWeight: "$forzaBook",
+  color: "White",
+  textAlign: "center",
+});
